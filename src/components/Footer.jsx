56ad4d775628ffa6ca102567148687ef0d7155d0
tanushrisolanki <tanushrisolanki827@gmail.com>
2025-01-09 22:37:07 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () =>{
+    return(
+        <footer>
+            Designed And Developed by @Elite Edge Fitness.
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const WorkoutSessions = () =>{
+    return(
+        <section className="workout_session">
+            <div className="wrapper">
+                <h1>TOP WORKOUT SESSION</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste illo, esse quas maxime eius vero modi provident minima magni, assumenda asperiores dolorum reprehenderit ut cumque tempore a rerum iusto totam.</p>
+                <img src="/img5.jpg" alt="workout" />
+            </div>
+            <div className="wrapper">
+                <h1>FEATURED BOOTCAMPS</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quaerat neque accusantium culpa debitis ea dignissimos aliquid impedit nihil voluptatibus non ad perferendis velit similique natus sapiente, aspernatur repudiandae nam!</p>
+                <div className="bootcamps">
+                    <div>
+                        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sint dicta a qui aspernatur aliquid error sapiente? Quasi esse porro, quia quaerat molestias nulla cum? Perspiciatis consequuntur modi incidunt architecto.</p>
+                    </div>
+                    <div>
+                        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sint dicta a qui aspernatur aliquid error sapiente? Quasi esse porro, quia quaerat molestias nulla cum? Perspiciatis consequuntur modi incidunt architecto.</p>
+                    </div>
+                    <div>
+                        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sint dicta a qui aspernatur aliquid error sapiente? Quasi esse porro, quia quaerat molestias nulla cum? Perspiciatis consequuntur modi incidunt architecto.</p>
+                    </div>
+                    <div>
+                        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sint dicta a qui aspernatur aliquid error sapiente? Quasi esse porro, quia quaerat molestias nulla cum? Perspiciatis consequuntur modi incidunt architecto.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default WorkoutSessions
